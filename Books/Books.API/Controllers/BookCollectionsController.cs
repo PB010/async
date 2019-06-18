3cdcc3940a2bcd2967fe27b169aebd6bf4e6f7a8
Petru Betco <petrubetco@gmail.com>
2019-06-18 17:29:01 +0300

    update

---
+++ Books/Books.API/Controllers/BookCollectionsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Books.Application.Books.Commands;
+using Books.Persistence.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.API.Controllers
+{
+    [Route("/api/bookCollections")]
+    [ApiController]
+    public class BookCollectionsController : BaseController
+    {
+        public BookCollectionsController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IEnumerable<Book>> AddMultipleBooks(
+            [FromBody] IEnumerable<AddNewBookCommand> command)
+        {
+
+        }
+    }
+}
